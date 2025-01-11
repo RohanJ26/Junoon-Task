b85346164e91d5dd43c476029155a3e9d51c8c4a
RohanJ26 <rohan.jhanwar@gmail.com>
2025-01-11 09:10:33 +0530

    Some changes

--- components/ui/Card.jsx
+++ components/ui/Card.jsx
@@ -1,22 +1,21 @@
-// components/ui/Card.jsx
 const Card = ({ children, className }) => {
     return <div className={`rounded-lg border p-4 shadow ${className}`}>{children}</div>;
-  };
+};
   
-  export const CardHeader = ({ children, className }) => {
+export const CardHeader = ({ children, className }) => {
     return <div className={`border-b pb-2 text-lg font-semibold ${className}`}>{children}</div>;
-  };
+};
   
-  export const CardContent = ({ children, className }) => {
+export const CardContent = ({ children, className }) => {
     return <div className={`py-2 ${className}`}>{children}</div>;
-  };
-  export const Skeleton = ({ className }) => {
+};
+export const Skeleton = ({ className }) => {
     return (
       <div
         className={`animate-pulse bg-gray-300 rounded ${className}`}
       ></div>
     );
-  };
+};
   
 export default Card;
   
